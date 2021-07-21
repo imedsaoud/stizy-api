ed80810798813118865a2458e58241506ca91293
awuzi <yahia.lamri@outlook.fr>
2021-07-21 14:01:09 +0200

    add place util

---
+++ utils/place.util.js
@@ -0,0 +1,27 @@
+/**
+ * type definition example
+ * The complete Triforce, or one or more components of the Triforce.
+ * @typedef {Object} Triforce
+ * @property {boolean} hasCourage - Indicates whether the Courage component is present.
+ * @property {boolean} hasPower - Indicates whether the Power component is present.
+ * @property {boolean} hasWisdom - Indicates whether the Wisdom component is present.
+ */
+
+const util = () => {
+
+    /**
+     * @param influxdbMetadata
+     * @param {Array<Place>} places
+     * @return {{mapPlaces: (function(*, *): (*|undefined))}}
+     */
+    const mapPlaces = (places, influxdbMetadata) => {
+
+    }
+
+    return {
+        mapPlaces
+    }
+
+}
+
+module.exports = util
