889c306c02a9e13ff3b2d5b6395da236c5ac0b75
Awuzi <yahia.lamri@outlook.fr>
2021-07-27 11:48:31 +0200

    fix user schema => remove role

---
+++ middleware/index.js
@@ -0,0 +1,24 @@
+// const jwt = require('jsonwebtoken');
+// const mongoose = require('mongoose');
+//
+// const verify = (req, res, next) => {
+//   const bearer = req.headers.authorization;
+//   if (!bearer) {
+//     res.sendStatus(401);
+//   } else {
+//     const token = bearer.replace("Bearer", "").trim();
+//     jwt.verify(token, config.jwtSecret, (err, user) => {
+//       if (err) {
+//         next({ httpCode: 401 });
+//       } else {
+//         user._id = mongoose.Types.ObjectId(user._id);
+//         req.user = user;
+//         next();
+//       }
+//     })
+//   }
+// }
+//
+// module.exports = {
+//   verify
+// };
